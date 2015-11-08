32ff47480a73d9bfc59c86716ee47b7483280560
Lui <injabie3@gmail.com>
2015-11-07 23:48:14 -0800

    Drafted Algorithm
    Drafted up an algorithm for findAndSetTokenDetails function of the class
    TokenList.
    
    Also tested string to int conversion from the following URL:
    http://stackoverflow.com/questions/194465/how-to-parse-a-string-to-an-int-in-c
    for into on string to int conversion
    
    test.cpp is just a testing source file.

---
+++ ENSC 251 Project/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string operators[31] = { "**", "abs", "not", "*", "/", "mod", "rem", "+", "-", "&", "sll", "srl", "sla", "sra", "rol", "ror", "=", "/=", "<", "<=", ">", ">=", "and", "or", "nand", "nor", "xor", "xnor" };
+
+	string number = "1";
+	int test = (int)number;
+	return 0;
+}
